4ef334f3d4c7275e7a0054c175ffbe76c84cee5f
Pablo Flores <pfloresphoto@hotmail.com>
2022-10-19 01:26:14 -0300

    Update
    Agregado estilo a paginas de enoturismo, historia y viñedos.
    Agregado script de slides para imagenes
    Agregado footer para index

--- ContactForm.js
+++ ContactForm.js
@@ -1,15 +1,14 @@
 
-function submitForm(){
+function submitForm() {
 
     const formName = document.getElementById('formName').value
     const formMail = document.getElementById('formMail').value
     const formPhone = document.getElementById("formPhone").value
     const formMessage = document.getElementById('formMessage').value
-    
+
 
     const submitButton = document.getElementById('submit')
-    if (formName.length && formMail.length && formPhone.length > 0) 
-    {
+    if (formName.length && formMail.length && formPhone.length > 0) {
         alert("Gracias, " + formName + "Su mensaje se ha enviado correctamente")
     }
 
--- Nav.js
+++ Nav.js
@@ -1,38 +1,34 @@
 
-    const header = document.createElement("header");
-   
-
-    header.setAttribute("id", "Navigation");
-    
-    const logo = document.createElement("h1");
-    logo.setAttribute("class","logo");
-    logo.innerHTML = "Luigi Rosca";
-    const nav = document.createElement("nav");
-    const inicio = createNavLink("Inicio");
-    const historia = createNavLink("Historia");
-    const vinedos = createNavLink("Nuestros Viñedos");
-    const vinos = createNavLink("Nuestros Vinos");
-    const Enoturismo = createNavLink("Enoturismo");
-
-    const contacto = createNavLink("Contacto");
-    
-    inicio.setAttribute("href", "index.html");
-    historia.setAttribute("href", "Historia.html");
-    vinedos.setAttribute("href", "Vinedos.html");
-    vinos.setAttribute("href", "Vinos.html");
-    Enoturismo.setAttribute("href", "Enoturismo.html")
-    contacto.setAttribute("href", "Contacto.html");
+const header = document.createElement("header");
 
 
-    document.body.appendChild(header);
-    header.appendChild(logo);
-    header.appendChild(nav);
-    nav.appendChild(inicio);
-    nav.appendChild(historia);
-    nav.appendChild(vinedos);
-    nav.appendChild(vinos);
-    nav.appendChild(Enoturismo);
-    nav.appendChild(contacto);
+header.setAttribute("id", "Navigation");
+
+const logo = document.createElement("h1");
+logo.setAttribute("class", "logo");
+logo.innerHTML = "Luigi Rosca";
+const nav = document.createElement("nav");
+const inicio = createNavLink("Inicio");
+const historia = createNavLink("Historia");
+const vinedos = createNavLink("Nuestros Viñedos");
+const Enoturismo = createNavLink("Enoturismo");
+const contacto = createNavLink("Contacto");
+
+inicio.setAttribute("href", "index.html");
+historia.setAttribute("href", "Historia.html");
+vinedos.setAttribute("href", "Vinedos.html");
+Enoturismo.setAttribute("href", "Enoturismo.html")
+contacto.setAttribute("href", "Contacto.html");
+
+
+document.body.appendChild(header);
+header.appendChild(logo);
+header.appendChild(nav);
+nav.appendChild(inicio);
+nav.appendChild(historia);
+nav.appendChild(vinedos);
+nav.appendChild(Enoturismo);
+nav.appendChild(contacto);
 
 function createNavLink(title) {
 
